0ea9e986ab2bd0c602e45916ec4430b7b092433f
超 <uncle.chao@live.cn>
2017-11-10 14:16:37 +0800

    Add page.

---
+++ src/pages/vehicleMonitor/monitorPage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Main from "../../components/system/main/main";
+
+
+export default class MonitorPage extends Component {
+  render() {
+    return (
+      <div>
+        <Main {...this.props}>
+          
+        </Main>
+      </div>
+    );
+  }
+}
