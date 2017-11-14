e4dbe4bf3bc396eaf6a81a2699619eb508dbb7c9
超 <uncle.chao@live.cn>
2017-11-14 11:12:34 +0800

    message dispatch completed.

--- src/pages/peopleDispatch/messageDispatchPage.jsx
+++ src/pages/peopleDispatch/messageDispatchPage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Tree, notification, message } from "antd";
+import { Button, Row, Col, Tree, Input, notification, message } from "antd";
 import Main from "../../components/system/main/main";
 
 export default class MessageDispatchPage extends Component {
@@ -9,7 +9,10 @@ export default class MessageDispatchPage extends Component {
       expandedKeys: [],
       autoExpandParent: true,
       checkedKeys: [],
-      treeData: []
+      treeData: [],
+      sendContent: "",
+      rewriteBtnDisable: true,
+      sendBtnDisable: true
     };
 
     fetch("/api/getOrganAndPeopleTreeList", {
@@ -52,8 +55,63 @@ export default class MessageDispatchPage extends Component {
     });
   };
   onCheck = checkedKeys => {
-    this.setState({ checkedKeys });
+    if (checkedKeys.length > 0 && this.state.rewriteBtnDisable === false) {
+      this.setState({ sendBtnDisable: false, checkedKeys });
+    } else {
+      this.setState({ sendBtnDisable: true, checkedKeys });
+    }
   };
+
+  onClear = () => {
+    this.setState({ sendContent: "", rewriteBtnDisable: true }, () => {
+      if (
+        this.state.checkedKeys.length > 0 &&
+        this.state.rewriteBtnDisable === false
+      ) {
+        this.setState({
+          sendBtnDisable: false,
+          sendContent: "",
+          rewriteBtnDisable: true
+        });
+      } else {
+        this.setState({
+          sendBtnDisable: true,
+          sendContent: "",
+          rewriteBtnDisable: true
+        });
+      }
+    });
+  };
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({ sendContent: e.target.value }, () => {
+      if (this.state.sendContent !== "") {
+        this.setState({ rewriteBtnDisable: false }, () => {
+          if (
+            this.state.checkedKeys.length > 0 &&
+            this.state.rewriteBtnDisable === false
+          ) {
+            this.setState({ sendBtnDisable: false });
+          } else {
+            this.setState({ sendBtnDisable: true });
+          }
+        });
+      } else {
+        this.setState({ rewriteBtnDisable: true }, () => {
+          if (
+            this.state.checkedKeys.length > 0 &&
+            this.state.rewriteBtnDisable === false
+          ) {
+            this.setState({ sendBtnDisable: false });
+          } else {
+            this.setState({ sendBtnDisable: true });
+          }
+        });
+      }
+    });
+  };
+
   renderTreeNodes = data => {
     return data.map(item => {
       if (item.children) {
@@ -71,16 +129,50 @@ export default class MessageDispatchPage extends Component {
     return (
       <div>
         <Main {...this.props}>
-          <Tree
-            checkable
-            onExpand={this.onExpand}
-            expandedKeys={this.state.expandedKeys}
-            autoExpandParent={this.state.autoExpandParent}
-            onCheck={this.onCheck}
-            checkedKeys={this.state.checkedKeys}
-          >
-            {this.renderTreeNodes(this.state.treeData)}
-          </Tree>
+          <Row>
+            <Col span={6}>
+              <p>请选择短信发送对象:</p>
+              <Tree
+                checkable
+                onExpand={this.onExpand}
+                expandedKeys={this.state.expandedKeys}
+                autoExpandParent={this.state.autoExpandParent}
+                onCheck={this.onCheck}
+                checkedKeys={this.state.checkedKeys}
+              >
+                {this.renderTreeNodes(this.state.treeData)}
+              </Tree>
+            </Col>
+            <Col span={18}>
+              <div style={{ margin: "0 20px 0 0" }}>
+                <p>请填写短信发送内容:</p>
+                <Input.TextArea
+                  placeholder="内容"
+                  autosize={{ minRows: 10, maxRows: 20 }}
+                  value={this.state.sendContent}
+                  onChange={this.handleChange.bind(this)}
+                  ref="sengContentInput"
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6} offset={18}>
+              <Button
+                style={{ margin: "16px" }}
+                onClick={this.onClear.bind(this)}
+                disabled={this.state.rewriteBtnDisable}
+              >
+                重新填写
+              </Button>
+              <Button
+                style={{ margin: "16px" }}
+                disabled={this.state.sendBtnDisable}
+              >
+                发送
+              </Button>
+            </Col>
+          </Row>
         </Main>
       </div>
     );
