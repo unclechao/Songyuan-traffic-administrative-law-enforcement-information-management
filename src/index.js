ea7c11fea8f3c9a464ad14df27770dfb3ba49094
超 <uncle.chao@live.cn>
2017-12-28 11:24:57 +0800

    add LocaleProvider.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap-theme.min.css";
 import registerServiceWorker from "./registerServiceWorker";
 import R from "./routes";
+import { LocaleProvider } from "antd";
+import zhCN from "antd/lib/locale-provider/zh_CN";
 
 ReactDOM.render(
-  <div>
-    <R />
-  </div>,
+  <LocaleProvider locale={zhCN}>
+    <div>
+      <R />
+    </div>
+  </LocaleProvider>,
   document.getElementById("root")
 );
 registerServiceWorker();
